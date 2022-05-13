4636c2a61b113a4833fefcf4b69a481ea4a86fdd
david-lafontant <lafontant.david@gmail.com>
2022-05-13 13:08:07 -0600

    Fix bugs

--- db/migrate/20220513190652_add_post_to_comments.rb
+++ db/migrate/20220513190652_add_post_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPostToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :user, null: false, foreign_key: true
+    add_reference :comments, :post, null: false, foreign_key: true
   end
 end
