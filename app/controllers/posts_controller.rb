4ddc669a782bc874f78a0fb2a103f0cc1895e07c
david-lafontant <lafontant.david@gmail.com>
2022-05-21 02:06:54 -0600

    Update posts and users controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
   end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+    else
+      render :new
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
