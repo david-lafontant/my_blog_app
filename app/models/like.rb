a67156c4314ebf6804846f40d37e69cde66ee8aa
david-lafontant <lafontant.david@gmail.com>
2022-06-02 18:32:48 -0600

    Update model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,9 +2,15 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
-  after_save :update_likes_counter
+  after_create :increment_likes_counter
+  after_destroy :decrement_likes_counter
 
-  def update_likes_counter
+  private def increment_likes_counter
     post.increment!(:likesCounter)
   end
+
+  private def decrement_likes_counter
+    post.decrement!(:likesCounter)
+  end
 end
+
