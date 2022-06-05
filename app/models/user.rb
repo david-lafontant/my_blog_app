fb2f24452bb557d82518637e6bc4f9bfb79f8068
david-lafontant <lafontant.david@gmail.com>
2022-06-04 22:09:03 -0600

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :confirmable
   has_many :posts, class_name: 'Post', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
 
   validates :name, presence: true
+  validates :bio, presence: true
+  validates :photo, presence: true
   validates :postsCounter, presence: true, numericality: { only_integer: true },
                            comparison: { greater_than_or_equal_to: 0 }
 
@@ -22,14 +24,20 @@ class User < ApplicationRecord
     posts.order(created_at: :desc)
   end
 
+  def generate_jwt
+    JWT.encode({ id:,
+                 exp: 60.days.from_now.to_i },
+               Rails.application.secrets.secret_key_base)
+  end
+
   private
 
   def default_values
     return unless new_record?
 
-    self.photo ||= 'user_photo.png'
+    self.bio ||= 'Enter your abstract bio here.'
+    self.photo ||= 'user-profile.png'
     self.postsCounter ||= 0
     self.role ||= 'default'
-    self.bio ||= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   end
 end
