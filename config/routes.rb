5e794d20154001333a3a4e6a6e6c8a7e3bbae7fa
david-lafontant <lafontant.david@gmail.com>
2022-06-04 22:05:40 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,20 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: %i[new create destroy] do
-    resources :comments, only: %i[new create destroy]
+    resources :comments, only: %i[index new create destroy]
     resources :likes, only: %i[create destroy]
+  end
+
+  namespace :api do
+    devise_for :users, as: 'api', controllers: { sessions: 'api/sessions', registrations: 'api/registrations' }
+
+    resources :users, only: %i[] do
+      resources :posts, only: %i[index]
+    end
+  
+    resources :posts, only: %i[] do
+      resources :comments, only: %i[index create]
+    end
   end
 
   # Defines the root path route ("/")
